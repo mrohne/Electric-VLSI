36f4b4b9ba8410c6a3eda7e8fc34d84cbb9616b6
Ole Rohne <ole.rohne@cern.ch>
2011-01-07 17:35:04 +0100

    Major fixes to DXF export

--- electric/electric-java/com/sun/electric/tool/io/output/DXF.java
+++ electric/electric-java/com/sun/electric/tool/io/output/DXF.java
@@ -24,14 +24,20 @@
 package com.sun.electric.tool.io.output;
 
 import com.sun.electric.database.geometry.Poly;
+import com.sun.electric.database.geometry.ERectangle;
+import com.sun.electric.database.geometry.EGraphics;
 import com.sun.electric.database.hierarchy.Cell;
 import com.sun.electric.database.prototype.NodeProto;
 import com.sun.electric.database.text.TextUtils;
 import com.sun.electric.database.topology.NodeInst;
+import com.sun.electric.database.topology.ArcInst;
+import com.sun.electric.database.topology.PortInst;
 import com.sun.electric.database.variable.VarContext;
 import com.sun.electric.database.variable.Variable;
+import com.sun.electric.database.variable.TextDescriptor;
 import com.sun.electric.technology.Layer;
 import com.sun.electric.technology.PrimitiveNode;
+import com.sun.electric.technology.ArcProto;
 import com.sun.electric.technology.Technology;
 import com.sun.electric.technology.technologies.Artwork;
 import com.sun.electric.tool.io.IOTool;
@@ -40,6 +46,7 @@ import com.sun.electric.util.math.FixpTransform;
 
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.awt.Color;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
@@ -95,15 +102,33 @@ public class DXF extends Output
 		// set the scale
 		dxfDispUnit = TextUtils.UnitScale.findFromIndex(localPrefs.dxfScale);
 
+		// get the bounding box
+		ERectangle bb = cell.getBounds();
+		double minX = TextUtils.convertDistance(bb.getMinX(), localPrefs.tech, dxfDispUnit);
+		double minY = TextUtils.convertDistance(bb.getMinY(), localPrefs.tech, dxfDispUnit);
+		double maxX = TextUtils.convertDistance(bb.getMaxX(), localPrefs.tech, dxfDispUnit);
+		double maxY = TextUtils.convertDistance(bb.getMaxY(), localPrefs.tech, dxfDispUnit);
+		
 		// write the header
+		printWriter.print("  0\nSECTION\n");
+		printWriter.print("  2\nHEADER\n");
+		printWriter.print("  9\n$LIMMIN\n");
+		printWriter.print(" 10\n" + TextUtils.formatDouble(minX) + "\n");
+		printWriter.print(" 20\n" + TextUtils.formatDouble(minY) + "\n");
+		printWriter.print("  9\n$LIMMAX\n");
+		printWriter.print(" 10\n" + TextUtils.formatDouble(maxX) + "\n");
+		printWriter.print(" 20\n" + TextUtils.formatDouble(maxY) + "\n");
+		printWriter.print("  9\n$INSUNITS\n");
+		if (dxfDispUnit.getMultiplier().doubleValue() == 1.000) printWriter.print(" 70\n6\n");
+		else if (dxfDispUnit.getMultiplier().doubleValue() == 0.001) printWriter.print(" 70\n4\n");
+		else if (dxfDispUnit.getMultiplier().doubleValue() == 1.e-6) printWriter.print(" 70\n13\n");
+		else printWriter.print(" 70\n0\n");
 		Variable varheadertext = cell.getLibrary().getVar(DXF_HEADER_TEXT_KEY);
 		Variable varheaderid = cell.getLibrary().getVar(DXF_HEADER_ID_KEY);
 		Layer defLay = Artwork.tech().findLayer("Graphics");
 		defaultDXFLayerName = defLay.getDXFLayer();
 		if (varheadertext != null && varheaderid != null)
 		{
-			printWriter.print("  0\nSECTION\n");
-			printWriter.print("  2\nHEADER\n");
 			int len = Math.min(varheadertext.getLength(), varheaderid.getLength());
 			for(int i=0; i<len; i++)
 			{
@@ -139,11 +164,34 @@ public class DXF extends Output
 
 				printWriter.print(getThreeDigit(code) + "\n" + pt + "\n");
 			}
-			printWriter.print("  0\nENDSEC\n");
 		}
+		printWriter.print("  0\nENDSEC\n");
+			
+		// write any tables
+		dxfEntityHandle = 0x100;
+		printWriter.print("  0\nSECTION\n");
+		printWriter.print("  2\nTABLES\n");
+		// VPORT
+		printWriter.print("  0\nTABLE\n");
+		printWriter.print("  5\n" + getThreeDigitHex(dxfEntityHandle++) + "\n");
+		printWriter.print("  2\nVPORT\n");
+		printWriter.print("  0\nVPORT\n");
+		printWriter.print("  5\n" + getThreeDigitHex(dxfEntityHandle++) + "\n");
+		printWriter.print("  2\n*Active\n");
+		printWriter.print(" 12\n" + TextUtils.formatDouble((minX+maxX)/2.0) + "\n");
+		printWriter.print(" 22\n" + TextUtils.formatDouble((minY+maxY)/2.0) + "\n");
+		printWriter.print(" 40\n" + TextUtils.formatDouble((maxX-minX)/1.0) + "\n");
+		printWriter.print("  0\nENDTAB\n");
+		// LAYER
+		printWriter.print("  0\nTABLE\n");
+		printWriter.print("  5\n" + getThreeDigitHex(dxfEntityHandle++) + "\n");
+		printWriter.print("  2\nLAYER\n");
+		for (Iterator<Layer> it = cell.getTechnology().getLayers(); it.hasNext(); )
+			writeDXFLayer(cell, it.next());
+		printWriter.print("  0\nENDTAB\n");
+		printWriter.print("  0\nENDSEC\n");
 
 		// write any subcells
-		dxfEntityHandle = 0x100;
 		printWriter.print("  0\nSECTION\n");
 		printWriter.print("  2\nBLOCKS\n");
 
@@ -157,8 +205,9 @@ public class DXF extends Output
 				if (!cellsSeen.contains(np)) writeDXFCell((Cell)np, true);
 			}
 		}
-
 		printWriter.print("  0\nENDSEC\n");
+
+		// write any entities
 		printWriter.print("  0\nSECTION\n");
 		printWriter.print("  2\nENTITIES\n");
 		writeDXFCell(cell, false);
@@ -187,11 +236,9 @@ public class DXF extends Output
 	 */
 	private void writeDXFCell(Cell cell, boolean subCells)
 	{
-		if (subCells)
-		{
+		if (subCells) {
 			cellsSeen.add(cell);
-			for(Iterator<NodeInst> it = cell.getNodes(); it.hasNext(); )
-			{
+			for(Iterator<NodeInst> it = cell.getNodes(); it.hasNext(); ) {
 				NodeInst ni = it.next();
 				NodeProto np = ni.getProto();
 				if (ni.isCellInstance() && !cellsSeen.contains(np))
@@ -205,17 +252,45 @@ public class DXF extends Output
 			printWriter.print(" 70\n0\n");
 		}
 
-		for(Iterator<NodeInst> it = cell.getNodes(); it.hasNext(); )
-		{
+		for(Iterator<ArcInst> it = cell.getArcs(); it.hasNext(); ) {
+			ArcInst ai = it.next();			
+			PortInst p0 = ai.getHeadPortInst();
+			PortInst p1 = ai.getTailPortInst();
+			// write line
+			writeDXFLine(cell, p0.getCenter(), p1.getCenter(), defaultDXFLayerName);
+			// write variables
+			Poly[] texts = ai.getDisplayableVariables(ai.getBounds(), null, false, false);
+			for(int i=0; i<texts.length; i++) {
+				Poly poly = texts[i];
+				writeDXFText(cell, poly, defaultDXFLayerName);
+			}
+			// write geometry
+			Poly [] polys = ai.getProto().getTechnology().getShapeOfArc(ai);
+			for(int i=0; i<polys.length; i++) {
+				Poly poly = polys[i];
+				Layer layer = poly.getLayer();
+				if (layer == null) {
+					errorLogger.logError("Null poly.getLayer()", poly, cell, dxfEntityHandle);
+					continue;
+				}
+				String layerName = defaultDXFLayerName;
+				if (layer.getDXFLayer() != null && layer.getDXFLayer().length() != 0)
+					layerName = layer.getDXFLayer();
+				writeDXFPoly(cell, poly, layerName);
+			}
+		}
+
+		for(Iterator<NodeInst> it = cell.getNodes(); it.hasNext(); ) {
 			NodeInst ni = it.next();
 			NodeProto np = ni.getProto();
-
-			// handle instances
-			if (ni.isCellInstance())
-			{
+			FixpTransform trans = ni.rotateOut();
+			String layerName = defaultDXFLayerName;
+			if (ni.getVar(DXF_LAYER_KEY) != null) layerName = ni.getVar(DXF_LAYER_KEY).getPureValue(-1);
+			// cell instance
+			if (ni.isCellInstance()) {
 				Cell subCell = (Cell)np;
 				printWriter.print("  0\nINSERT\n");
-				printWriter.print("  8\nUNKNOWN\n");
+				printWriter.print("  8\n" + defaultDXFLayerName + "\n");
 				printWriter.print("  5\n" + getThreeDigitHex(dxfEntityHandle++) + "\n");
 				printWriter.print("  2\n" + getDXFCellName(subCell) + "\n");
 				Rectangle2D cellBounds = subCell.getBounds();
@@ -228,82 +303,175 @@ public class DXF extends Output
 				printWriter.print(" 50\n" + formatDoubleJavaVersion(rot) + "\n");
 				continue;
 			}
-
-			// determine layer name for this node
-			String layerName = defaultDXFLayerName;
-			Variable var = ni.getVar(DXF_LAYER_KEY);
-			if (var != null) layerName = var.getPureValue(-1); else
-			{
-				// examine technology for proper layer name
-				if (!ni.isCellInstance())
-				{
-					PrimitiveNode pNp = (PrimitiveNode)ni.getProto();
-					Technology.NodeLayer [] nodeLayers = pNp.getNodeLayers();
-					for(int i=0; i<nodeLayers.length; i++)
-					{
-						Technology.NodeLayer nl = nodeLayers[i];
-						Layer layer = nl.getLayer();
-						String dxfLayerName = layer.getDXFLayer();
-						if (dxfLayerName != null && dxfLayerName.length() > 0)
-						{
-							layerName = dxfLayerName;
-							break;
-						}
-					}
-				}
+			// write variables
+			Poly[] texts = ni.getDisplayableVariables(ni.getBounds(), null, false, false);
+			for(int i=0; i<texts.length; i++) {
+				Poly poly = texts[i];
+				writeDXFText(cell, poly, defaultDXFLayerName);
 			}
+			// write geometry
+			Poly [] polys = ni.getProto().getTechnology().getShapeOfNode(ni);
+			for(int i=0; i<polys.length; i++) {
+				Poly poly = polys[i];
+				Layer layer = poly.getLayer();
+				if (layer == null) {
+					errorLogger.logError("Null poly.getLayer()", poly, cell, dxfEntityHandle);
+					continue;
+				}
+				if (layer.getDXFLayer() != null && layer.getDXFLayer().length() != 0) layerName = layer.getDXFLayer();
+				poly.transform(trans);
+				writeDXFPoly(cell, poly, layerName);
+			}
+		}
+		if (subCells) printWriter.print("  0\nENDBLK\n");
+	}
 
-			// get the node center
-			double xC = ni.getAnchorCenterX();
-			double yC = ni.getAnchorCenterY();
+	private void writeDXFLayer (Cell cell, Layer layer) {
+		// traverse the standard palette
+		int red = layer.getGraphics().getColor().getRed();
+		int green = layer.getGraphics().getColor().getGreen();
+		int blue = layer.getGraphics().getColor().getBlue();
+		float hsb[] = Color.RGBtoHSB(red, green, blue, null);
+		int hsbcol = (int)(10+10*Math.floor(24*hsb[0])+1*Math.floor(5*(1.0-hsb[1])+5*(1.0-hsb[2])));
+		int rgbcol = (red<<16|green<<8|blue<<0);
+		printWriter.print("  0\nLAYER\n");
+		printWriter.print("  2\n" + layer.getDXFLayer() + "\n");
+		printWriter.print("  5\n" + getThreeDigitHex(dxfEntityHandle++) + "\n");
+		printWriter.print(" 62\n" + hsbcol + "\n");
+		printWriter.print("420\n" + rgbcol + "\n");
+	}
 
-			// handle circles and arcs
-			if (np instanceof PrimitiveNode && ((PrimitiveNode)np).isPartialCircle())
-			{
-				double [] angles = ni.getArcDegrees();
-				double startOffset = angles[0];
-				double endAngle = angles[1];
-				if (startOffset != 0.0 || endAngle != 0.0) printWriter.print("  0\nARC\n"); else
-					printWriter.print("  0\nCIRCLE\n");
-				printWriter.print("  8\n" + layerName + "\n");
+
+	private void writeDXFText (Cell cell, Poly poly, String layer) {
+		// extract data
+		TextDescriptor desc = poly.getTextDescriptor();
+		String text = poly.getString();
+		double xC = TextUtils.convertDistance(poly.getCenterX(), localPrefs.tech, dxfDispUnit);
+		double yC = TextUtils.convertDistance(poly.getCenterY(), localPrefs.tech, dxfDispUnit);
+		double yH = TextUtils.convertDistance(desc.getSize().getSize(), localPrefs.tech, dxfDispUnit);
+		// ship out entity
+		printWriter.print("  0\nTEXT\n");
+		printWriter.print("  1\n" + text +"\n");
+		printWriter.print("  5\n" + getThreeDigitHex(dxfEntityHandle++) + "\n");
+		printWriter.print("  8\n" + layer + "\n");
+		printWriter.print(" 10\n" + formatDoubleJavaVersion(xC) + "\n");
+		printWriter.print(" 20\n" + formatDoubleJavaVersion(yC) + "\n");
+		printWriter.print(" 11\n" + formatDoubleJavaVersion(xC) + "\n");
+		printWriter.print(" 21\n" + formatDoubleJavaVersion(yC) + "\n");
+		printWriter.print(" 40\n" + formatDoubleJavaVersion(yH) + "\n");
+		printWriter.print(" 50\n" + formatDoubleJavaVersion(desc.getRotation().getAngle()) + "\n");
+		// switch over horizontal alignment
+		switch (poly.getStyle()) {
+		case TEXTLEFT:
+		case TEXTTOPLEFT:
+		case TEXTBOTLEFT:
+			printWriter.print(" 72\n0\n"); break;
+		case TEXTBOX:
+		case TEXTCENT:
+		case TEXTTOP:
+		case TEXTBOT:
+			printWriter.print(" 72\n1\n"); break;
+		case TEXTRIGHT:
+		case TEXTTOPRIGHT:
+		case TEXTBOTRIGHT:
+			printWriter.print(" 72\n2\n"); break;
+		}
+		// switch on vertical alignment
+		switch (poly.getStyle()) {
+		case TEXTBOX:
+		case TEXTCENT:
+			printWriter.print(" 73\n0\n"); break;
+		case TEXTBOT:
+		case TEXTBOTLEFT:
+		case TEXTBOTRIGHT:
+			printWriter.print(" 73\n1\n"); break;
+		case TEXTLEFT:
+		case TEXTRIGHT:
+			printWriter.print(" 73\n2\n"); break;
+		case TEXTTOP:
+		case TEXTTOPLEFT:
+		case TEXTTOPRIGHT:
+			printWriter.print(" 73\n3\n"); break;
+		}
+	}
+
+	private void writeDXFLine (Cell cell, Point2D p0, Point2D p1, String layer) {
+		// extract data
+		double x0 = TextUtils.convertDistance(p0.getX(), localPrefs.tech, dxfDispUnit);
+		double y0 = TextUtils.convertDistance(p0.getY(), localPrefs.tech, dxfDispUnit);
+		double x1 = TextUtils.convertDistance(p1.getX(), localPrefs.tech, dxfDispUnit);
+		double y1 = TextUtils.convertDistance(p1.getY(), localPrefs.tech, dxfDispUnit);
+		// ship out entity
+		printWriter.print("  0\nLINE\n");
+		printWriter.print("  5\n" + getThreeDigitHex(dxfEntityHandle++) + "\n");
+		printWriter.print("  8\n" + layer + "\n");
+		printWriter.print(" 10\n" + formatDoubleJavaVersion(x0) + "\n");
+		printWriter.print(" 20\n" + formatDoubleJavaVersion(y0) + "\n");
+		printWriter.print(" 11\n" + formatDoubleJavaVersion(x1) + "\n");
+		printWriter.print(" 21\n" + formatDoubleJavaVersion(y1) + "\n");
+	}
+
+	private void writeDXFPoly (Cell cell, Poly poly, String layer) {
+		// extract data
+		Point2D [] points = poly.getPoints();
+		if (points.length == 0) {
+			errorLogger.logError("Null poly.Points()", poly, cell, dxfEntityHandle);
+			return;
+		}
+		// ship out entity
+		switch (poly.getStyle()) {
+		case CROSS:
+		case BIGCROSS:
+			if (points.length >= 1) {
+				double x0 = TextUtils.convertDistance(points[0].getX(), localPrefs.tech, dxfDispUnit);
+				double y0 = TextUtils.convertDistance(points[0].getY(), localPrefs.tech, dxfDispUnit);
+				printWriter.print("  0\nPOINT\n");
 				printWriter.print("  5\n" + getThreeDigitHex(dxfEntityHandle++) + "\n");
-				xC = TextUtils.convertDistance(xC, localPrefs.tech, dxfDispUnit);
-                yC = TextUtils.convertDistance(yC, localPrefs.tech, dxfDispUnit);
-				double zC = TextUtils.convertDistance(ni.getYSize() / 2, localPrefs.tech, dxfDispUnit);
-				printWriter.print(" 10\n" + formatDoubleJavaVersion(xC) + "\n");
-				printWriter.print(" 20\n" + formatDoubleJavaVersion(yC) + "\n");
-				printWriter.print(" 30\n0\n");
-				printWriter.print(" 40\n" + formatDoubleJavaVersion(zC) + "\n");
-				if (startOffset != 0.0 || endAngle != 0.0)
-				{
-					startOffset = startOffset * 180.0 / Math.PI;
-					endAngle = endAngle * 180.0 / Math.PI;
-					double startAngle = ni.getAngle() / 10.0 + startOffset;
-					if (ni.isXMirrored() != ni.isYMirrored())
-					{
-						startAngle = 270.0 - startAngle - endAngle;
-						if (startAngle < 0.0) startAngle += 360.0;
-					}
-					endAngle += startAngle;
-					if (endAngle >= 360.0) endAngle -= 360.0;
-					printWriter.print(" 50\n" + formatDoubleJavaVersion(startAngle) + "\n");
-					printWriter.print(" 51\n" + formatDoubleJavaVersion(endAngle) + "\n");
-				}
-				continue;
+				printWriter.print("  8\n" + layer + "\n");
+				printWriter.print(" 10\n" + formatDoubleJavaVersion(x0) + "\n");
+				printWriter.print(" 20\n" + formatDoubleJavaVersion(y0) + "\n");
 			}
-
-			// handle polygons
-			if (np == Artwork.tech().openedPolygonNode || np == Artwork.tech().openedDashedPolygonNode ||
-				np == Artwork.tech().closedPolygonNode)
-			{
-				FixpTransform trans = ni.rotateOut();
-				Point2D [] points = ni.getTrace();
-				int len = points.length;
-				if (len == 2 && np != Artwork.tech().closedPolygonNode)
-				{
-					// line
+			else errorLogger.logError("Malformed POINT", poly, cell, dxfEntityHandle);
+			break;
+		case DISC:
+		case CIRCLE:
+		case THICKCIRCLE:
+			if (points.length == 2) {
+				double x0 = TextUtils.convertDistance(points[0].getX(), localPrefs.tech, dxfDispUnit);
+				double y0 = TextUtils.convertDistance(points[0].getY(), localPrefs.tech, dxfDispUnit);
+				double r0 = TextUtils.convertDistance(points[0].distance(points[1]), localPrefs.tech, dxfDispUnit);
+				printWriter.print("  0\nCIRCLE\n");
+				printWriter.print("  5\n" + getThreeDigitHex(dxfEntityHandle++) + "\n");
+				printWriter.print("  8\n" + layer + "\n");
+				printWriter.print(" 10\n" + formatDoubleJavaVersion(x0) + "\n");
+				printWriter.print(" 20\n" + formatDoubleJavaVersion(y0) + "\n");
+				printWriter.print(" 40\n" + formatDoubleJavaVersion(r0) + "\n");
+			}
+			else errorLogger.logError("Malformed CIRCLE", poly, cell, dxfEntityHandle);
+			break;
+		case CIRCLEARC:
+		case THICKCIRCLEARC:
+			if (points.length == 3) {	
+				double x0 = TextUtils.convertDistance(points[0].getX(), localPrefs.tech, dxfDispUnit);
+				double y0 = TextUtils.convertDistance(points[0].getY(), localPrefs.tech, dxfDispUnit);
+				double r0 = TextUtils.convertDistance(points[0].distance(points[1]), localPrefs.tech, dxfDispUnit);
+				double a0 = DBMath.figureAngle(points[0], points[2])/10;
+				double a1 = DBMath.figureAngle(points[0], points[1])/10;
+				printWriter.print("  0\nARC\n");
+				printWriter.print("  5\n" + getThreeDigitHex(dxfEntityHandle++) + "\n");
+				printWriter.print("  8\n" + layer + "\n");
+				printWriter.print(" 10\n" + formatDoubleJavaVersion(x0) + "\n");
+				printWriter.print(" 20\n" + formatDoubleJavaVersion(y0) + "\n");
+				printWriter.print(" 40\n" + formatDoubleJavaVersion(r0) + "\n");
+				printWriter.print(" 50\n" + formatDoubleJavaVersion(a0) + "\n");
+				printWriter.print(" 51\n" + formatDoubleJavaVersion(a1) + "\n");
+			}
+			else errorLogger.logError("Malformed ARC", poly, cell, dxfEntityHandle);
+			break;
+		case VECTORS:
+			if (points.length % 2 == 0) {
+				for (int i = 0; i < points.length; i += 2) {
 					printWriter.print("  0\nLINE\n");
-					printWriter.print("  8\n" + layerName + "\n");
 					printWriter.print("  5\n" + getThreeDigitHex(dxfEntityHandle++) + "\n");
 					Point2D pt = new Point2D.Double(points[0].getX() + xC, points[0].getY() + yC);
 					trans.transform(pt, pt);
@@ -365,7 +533,6 @@ public class DXF extends Output
 						printWriter.print(" 31\n0\n");
 					}
 				}
-				continue;
 			}
 
 			// write all other nodes
@@ -387,10 +554,14 @@ public class DXF extends Output
 						printWriter.print(" 3" + j + "\n0\n");
 					}
 				}
+				printWriter.print("  0\nSEQEND\n");
 			}
+			else errorLogger.logError("Malformed POLYLINE", poly, cell, dxfEntityHandle);
+			break;
+		default:
+			errorLogger.logError("Please add unknown poly", poly, cell, dxfEntityHandle);
+			break;
 		}
-		if (subCells)
-			printWriter.print("  0\nENDBLK\n");
 	}
 
 	private String getDXFCellName(Cell cell)
