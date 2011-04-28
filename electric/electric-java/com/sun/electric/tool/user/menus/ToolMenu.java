de5c761a973ab3e72aa4e5f45be9830f8b6e8bda
Ole Rohne <ole.rohne@cern.ch>
2011-04-28 08:38:22 +0200

    Fixed running ABCL-script from the dynamic menu

--- electric/electric-java/com/sun/electric/tool/user/menus/ToolMenu.java
+++ electric/electric-java/com/sun/electric/tool/user/menus/ToolMenu.java
@@ -2408,7 +2408,7 @@ public class ToolMenu {
 				EvalJython.runScript(fileName);
 			} else if (type == FileType.ABCL) {
 				System.out.println("Executing commands in ABCL Script: " + fileName);
-				EvalABCL.runScript(fileName);
+	        	EvalABCL.runScriptNoJob("(LOAD #p\""+fileName+"\")");
 			}
 		}
 	}
