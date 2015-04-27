0d990b7b31b198af5e07b6735259bd8a3d938659
Ole Rohne <ole.rohne@gmail.com>
2015-04-27 11:34:20 +0200

    On branch 0/build/1/osx - housekeeping 	modified:   build.xml 	modified:   debug.sh 	new file:   gud-jdb.el

--- electric/packaging/debug.sh
+++ electric/packaging/debug.sh
@@ -1,1 +1,3 @@
-jdb -launch -sourcepath../electric-java -classpathantBuild -Dapple.laf.useScreenMenuBar=true -Dcom.apple.mrj.application.apple.menu.about.name=Electric -J-Xms64m -J-Xmx2048m -J-Xss64m com.sun.electric.Launcher
+# jdb -attach 8000 -sourcepath../electric-java
+# jdb -launch -sourcepath../electric-java -classpathantBuild -Dapple.laf.useScreenMenuBar=true -Dcom.apple.mrj.application.apple.menu.about.name=Electric -J-Xms64m -J-Xmx2048m -J-Xss64m com.sun.electric.Launcher
+jdb -launch -sourcepath../electric-java -classpathantBuild:gluegen-rt-natives-macosx-universal.jar:gluegen-rt.jar:jogl-all-natives-macosx-universal.jar:jogl-all.jar -J-Xms64m -J-Xmx2048m -J-Xss64m -Dapple.laf.useScreenMenuBar=true -Dcom.apple.mrj.application.apple.menu.about.name=Electric com.sun.electric.Launcher
