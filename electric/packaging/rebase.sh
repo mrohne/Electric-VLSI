f627664a461d3d196384ee846e7c3c268f855426
Ole Rohne <ole.rohne@gmail.com>
2016-10-18 07:06:22 +0200

    On branch 0/build/0/git - updated rebase procedure

--- electric/packaging/rebase.sh
+++ electric/packaging/rebase.sh
@@ -7,3 +7,8 @@ git branch -M merged merged.old
 git checkout -b merged master
 git branch | grep / | sort | while read b; do echo Merging $b; git merge $b; done
 git stash pop
+git commit -am"Mirror only, please undo"
+git push --mirror mirror
+git push --mirror mirror
+git push --mirror mirror
+git reset --soft HEAD~1
