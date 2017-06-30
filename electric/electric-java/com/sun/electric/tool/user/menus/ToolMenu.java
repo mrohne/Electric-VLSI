def5768d3d614675cefbc2f0ef4ba54ac7ec0b36
Ole Rohne <ole.rohne@gmail.com>
2017-06-30 10:46:25 +0200

    On branch 1/lang/1/abcl -- moved fixes in ToolMenu.java

--- electric/electric-java/com/sun/electric/tool/user/menus/ToolMenu.java
+++ electric/electric-java/com/sun/electric/tool/user/menus/ToolMenu.java
@@ -191,7 +191,7 @@ public class ToolMenu {
 				public void run() { new LanguageScripts(); }
 			},
 			SEPARATOR);
-	
+
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() { setDynamicLanguageMenu(); }
 		});
@@ -2331,7 +2331,7 @@ public class ToolMenu {
 	public static void setDynamicLanguageMenu() {
 		for (EMenuBar.Instance menuBarInstance : TopLevel.getMenuBars()) {
 			JMenu menu = (JMenu) menuBarInstance.findMenuItem(languageMenu.getPath());
-			while (menu.getMenuComponentCount() > 3)
+			while (menu.getMenuComponentCount() > 4)
 				menu.remove(menu.getMenuComponentCount() - 1);
 
 			for (LanguageScripts.ScriptBinding script : LanguageScripts.getScripts()) {
