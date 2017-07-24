8df5ea36768e89aca74ece8b6881c71e0ab8bf33
Ole Rohne <ole.rohne@gmail.com>
2017-07-24 13:06:20 +0200

    On branch 0/build/0/git - separate rebase.sh and mirror.sh

--- electric/packaging/rebase.sh
+++ electric/packaging/rebase.sh
@@ -1,14 +1,10 @@
 git stash
 git checkout master
 git svn rebase
-git branch | grep / | sort | while read b; do echo Rebasing $b; git rebase master $b; done
+git branch | grep / | sort | while read b; do echo Rebasing $b; git rebase --onto master master $b; done
 git branch -D merged.old
 git branch -M merged merged.old
 git checkout -b merged master
 git branch | grep / | sort | while read b; do echo Merging $b; git merge $b; done
 git stash pop
-git commit -am"Mirror only, please undo"
-git push --mirror mirror
-git push --mirror mirror
-git push --mirror mirror
-git reset --soft HEAD~1
+
