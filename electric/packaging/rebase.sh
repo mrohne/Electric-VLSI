47f8525855ad6509a02ebefe45c8ac749d3513d8
Ole Rohne <o.m.rohne@fys.uio.no>
2020-12-28 11:13:48 +0100

    Rewrote toplevel README.org On branch 0/build/0/git 	modified:   README.org 	modified:   electric/packaging/rebase.sh

--- electric/packaging/rebase.sh
+++ electric/packaging/rebase.sh
@@ -1,10 +1,10 @@
 git stash
 git checkout master
 git svn rebase
-git branch | grep / | sort | while read b; do echo Rebasing $b; git rebase --onto master master $b; done
+git branch | grep / | sort | while read b; do echo Rebasing $b; git rebase --onto master master $b || break; done
 git branch -D merged.old
 git branch -M merged merged.old
 git checkout -b merged master
-git branch | grep / | sort | while read b; do echo Merging $b; git merge $b; done
+git branch | grep / | sort | while read b; do echo Merging $b; git merge $b || break; done
 git stash pop
 
